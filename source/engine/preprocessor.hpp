b4d88a8228f6a907603c5032cf8c792875f771b8
thealing <159199097+thealing@users.noreply.github.com>
2026-09-21 01:50:19 +0200

    Implemented undefinition

--- source/engine/preprocessor.hpp
+++ source/engine/preprocessor.hpp
@@ -139,6 +139,11 @@ public:
 
 			const auto& [par_list, replacement, replace_content] = *definition;
 
+			if (replacement.empty())
+			{
+				continue;
+			}
+
 			auto par_it = par_list.begin();
 
 			auto par_end = par_list.end();
