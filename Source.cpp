bcff0dd46dff98d6b11dd6f9d608e7ef9cbaf35c
thealing <159199097+thealing@users.noreply.github.com>
2026-09-21 02:34:03 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,6 @@
 #include "source/source.hpp"
 
-int main(int argc, char** argv)
+int main1(int argc, char** argv)
 {
 	std::vector<std::string> arguments(argv + 1, argv + argc);
 
@@ -13,5 +13,18 @@ int main(int argc, char** argv)
 	catch (int exit_code)
 	{
 		return exit_code;
+	}
+}
+
+int main()
+{
+	for (int i = 0; i < 10; i++) {
+		int x = clock();
+		std::vector<std::string> arguments;
+
+		arguments.push_back(R"(C:\Users\user\source\repos\DTC\build\x64-debug\generated\build.c)");
+		arguments.push_back(R"(C:\Users\user\source\repos\DTC\_\benchmark.c)");
+		cli_run(arguments);
+		std::cout << "time " << clock() - x << std::endl;
 	}
 }
